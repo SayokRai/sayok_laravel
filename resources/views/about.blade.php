394b17f895253f40d42da10b72df2374d7d72610
SayokRai <sayokrai2009@gmail.com>
2025-05-14 11:50:42 +0545

    laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+<body>
+    <header>
+        <x-navbar/>    
+    </header>
+    <h1>hello,welcome here</h1>
+    <a href="/home">go to home page</a>
+</body>
+</html>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,17 @@
+
+<nav class="flex items-center justify-between bg-teal-500 p-6">
+    <h1 class="text-[white] text-[20px]">
+        Sayok Rai
+    </h1>
+    <ul class="flex space-x-4">
+        <li>
+            <a href="/home">Home</a>        
+        </li>
+        <li>
+            <a href="/about">About</a>
+        </li>
+        <li>
+            <a href="">Contact</a>
+        </li>
+    </ul>
+</nav>
